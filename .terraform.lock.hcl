546c910070d1214992e718ab01c880d95de4525f
Thada Wangthammang <thada.wth@gmail.com>
2025-11-11 22:11:10 +0700

    Add initial Terraform configuration with VNet, VMs, and VPN gateways

---
+++ .terraform.lock.hcl
@@ -0,0 +1,62 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "4.52.0"
+  constraints = ">= 4.52.0"
+  hashes = [
+    "h1:1W7k+jiFVn1ufjpSLmIAe5C3rIalS0I0nBF6ErVRQgU=",
+    "zh:0044d786ddbd6160150eef0716d1700839526200ce96cf1dc57327e2c923876e",
+    "zh:1a4db8a19a32e7e50860f31a5d2f8dc9ee247089475b4b8b0b365e5bbd5e7647",
+    "zh:1ce0c9da46ead459fe146d5b9e993ea60484506c6f5b988725c0c24af11dc53e",
+    "zh:3fa21d310ae29d3b7456447cf7316bf9659ed62158debc80df1296c6a2d0721f",
+    "zh:4944391a9751d3bc47c3d6a8c15616d3994e35f2af2e58ef56373aae560af0de",
+    "zh:745d0079473f5803dccfe4bf29292ddfe0266ee2f2ad7a80823e4f10514ddeb7",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:a8d92d4bbdd7204c54df26afc9989f32ed457735bda2e767fdd3d54935886597",
+    "zh:b5b915850fcafe7f249a4c1af64ebb3acd1ea57f268831e8d605d585793b3ea9",
+    "zh:d3784bd8813498dd70ccf6d0248a8978512d33cfe4d64bd80801345b8287e2fd",
+    "zh:e5c4b4c5740a4cb68324933eb08f7628ed1ab292bcc8251a585b9adeb0c645a6",
+    "zh:f2621b68ccd17e07fe00ce21c4a6f9680e7456decca7509d206082af6d2a1c2f",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.7.2"
+  constraints = ">= 3.6.0"
+  hashes = [
+    "h1:KG4NuIBl1mRWU0KD/BGfCi1YN/j3F7H4YgeeM7iSdNs=",
+    "zh:14829603a32e4bc4d05062f059e545a91e27ff033756b48afbae6b3c835f508f",
+    "zh:1527fb07d9fea400d70e9e6eb4a2b918d5060d604749b6f1c361518e7da546dc",
+    "zh:1e86bcd7ebec85ba336b423ba1db046aeaa3c0e5f921039b3f1a6fc2f978feab",
+    "zh:24536dec8bde66753f4b4030b8f3ef43c196d69cccbea1c382d01b222478c7a3",
+    "zh:29f1786486759fad9b0ce4fdfbbfece9343ad47cd50119045075e05afe49d212",
+    "zh:4d701e978c2dd8604ba1ce962b047607701e65c078cb22e97171513e9e57491f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7b8434212eef0f8c83f5a90c6d76feaf850f6502b61b53c329e85b3b281cba34",
+    "zh:ac8a23c212258b7976e1621275e3af7099e7e4a3d4478cf8d5d2a27f3bc3e967",
+    "zh:b516ca74431f3df4c6cf90ddcdb4042c626e026317a33c53f0b445a3d93b720d",
+    "zh:dc76e4326aec2490c1600d6871a95e78f9050f9ce427c71707ea412a2f2f1a62",
+    "zh:eac7b63e86c749c7d48f527671c7aee5b4e26c10be6ad7232d6860167f99dbb0",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "4.1.0"
+  constraints = ">= 4.0.5"
+  hashes = [
+    "h1:zEv9tY1KR5vaLSyp2lkrucNJ+Vq3c+sTFK9GyQGLtFs=",
+    "zh:14c35d89307988c835a7f8e26f1b83ce771e5f9b41e407f86a644c0152089ac2",
+    "zh:2fb9fe7a8b5afdbd3e903acb6776ef1be3f2e587fb236a8c60f11a9fa165faa8",
+    "zh:35808142ef850c0c60dd93dc06b95c747720ed2c40c89031781165f0c2baa2fc",
+    "zh:35b5dc95bc75f0b3b9c5ce54d4d7600c1ebc96fbb8dfca174536e8bf103c8cdc",
+    "zh:38aa27c6a6c98f1712aa5cc30011884dc4b128b4073a4a27883374bfa3ec9fac",
+    "zh:51fb247e3a2e88f0047cb97bb9df7c228254a3b3021c5534e4563b4007e6f882",
+    "zh:62b981ce491e38d892ba6364d1d0cdaadcee37cc218590e07b310b1dfa34be2d",
+    "zh:bc8e47efc611924a79f947ce072a9ad698f311d4a60d0b4dfff6758c912b7298",
+    "zh:c149508bd131765d1bc085c75a870abb314ff5a6d7f5ac1035a8892d686b6297",
+    "zh:d38d40783503d278b63858978d40e07ac48123a2925e1a6b47e62179c046f87a",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:fb07f708e3316615f6d218cec198504984c0ce7000b9f1eebff7516e384f4b54",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,398 @@
+############################################################
+# main.tf — 2 VNets, 2 Ubuntu VMs, 2 VPN Gateways (VNet2VNet)
+# Terraform: >= 1.6.0
+# Provider : azurerm >= 4.0.0
+############################################################
+
+terraform {
+  required_version = ">= 1.13.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.52.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Config
+#####################
+locals {
+  location       = "southeastasia"
+  admin_username = "azureuser"
+  ssh_key_bits   = 4096
+
+  # Addressing
+  vnet1_cidr       = "10.10.0.0/16"
+  vnet1_subnet_vm  = "10.10.1.0/24"
+  vnet1_subnet_gw  = "10.10.255.0/27" # must be named GatewaySubnet
+
+  vnet2_cidr       = "10.20.0.0/16"
+  vnet2_subnet_vm  = "10.20.1.0/24"
+  vnet2_subnet_gw  = "10.20.255.0/27"
+
+  # Shared key for VNet2VNet connection
+  ipsec_psk = "Demo-ChangeMe-123!"
+}
+
+#####################
+# SSH key (for both VMs)
+#####################
+resource "tls_private_key" "vm" {
+  algorithm = "RSA"
+  rsa_bits  = local.ssh_key_bits
+}
+
+#####################
+# Resource groups
+#####################
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg-vnet1"
+  location = local.location
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg-vnet2"
+  location = local.location
+}
+
+#####################
+# VNet 1 + Subnets
+#####################
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = [local.vnet1_cidr]
+}
+
+resource "azurerm_subnet" "vnet1_vm" {
+  name                 = "snet-vm"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [local.vnet1_subnet_vm]
+}
+
+resource "azurerm_subnet" "vnet1_gw" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [local.vnet1_subnet_gw]
+}
+
+#####################
+# VNet 2 + Subnets
+#####################
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  address_space       = [local.vnet2_cidr]
+}
+
+resource "azurerm_subnet" "vnet2_vm" {
+  name                 = "snet-vm"
+  resource_group_name  = azurerm_resource_group.rg2.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = [local.vnet2_subnet_vm]
+}
+
+resource "azurerm_subnet" "vnet2_gw" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rg2.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = [local.vnet2_subnet_gw]
+}
+
+#####################
+# NSGs (allow SSH + ICMP inside VNet)
+#####################
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg-vnet1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-icmp-from-vnet"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "nsg-vnet2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-icmp-from-vnet"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+#####################
+# NICs + Public IPs + VMs (Ubuntu 22.04)
+#####################
+# VNet1
+resource "azurerm_public_ip" "vm1_pip" {
+  name                = "pip-vm1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm1_nic" {
+  name                = "nic-vm1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = "ipcfg"
+    subnet_id                     = azurerm_subnet.vnet1_vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vm1_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.vm1_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  size                = "Standard_B1s"
+  admin_username      = local.admin_username
+  network_interface_ids = [azurerm_network_interface.vm1_nic.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = local.admin_username
+    public_key = tls_private_key.vm.public_key_openssh
+  }
+
+  os_disk {
+    name                 = "osdisk-vm1"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# VNet2
+resource "azurerm_public_ip" "vm2_pip" {
+  name                = "pip-vm2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm2_nic" {
+  name                = "nic-vm2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+
+  ip_configuration {
+    name                          = "ipcfg"
+    subnet_id                     = azurerm_subnet.vnet2_vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm2_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vm2_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.vm2_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg2.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "vm2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  size                = "Standard_B1s"
+  admin_username      = local.admin_username
+  network_interface_ids = [azurerm_network_interface.vm2_nic.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = local.admin_username
+    public_key = tls_private_key.vm.public_key_openssh
+  }
+
+  os_disk {
+    name                 = "osdisk-vm2"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+#####################
+# VPN Gateways (VNet1 & VNet2) — azurerm v4
+#####################
+# Public IPs for Gateways — MUST be Standard + Static in v4
+resource "azurerm_public_ip" "gw1_pip" {
+  name                = "pip-gw1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_public_ip" "gw2_pip" {
+  name                = "pip-gw2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_virtual_network_gateway" "gw1" {
+  name                = "vngw1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  type       = "Vpn"
+  vpn_type   = "RouteBased"
+  sku        = "VpnGw1"
+  generation = "Generation2"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.gw1_pip.id
+    subnet_id                     = azurerm_subnet.vnet1_gw.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_network_gateway" "gw2" {
+  name                = "vngw2"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+
+  type       = "Vpn"
+  vpn_type   = "RouteBased"
+  sku        = "VpnGw1"
+  generation = "Generation2"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.gw2_pip.id
+    subnet_id                     = azurerm_subnet.vnet2_gw.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#####################
+# VNet-to-VNet Connections (bidirectional)
+#####################
+resource "azurerm_virtual_network_gateway_connection" "gw1_to_gw2" {
+  name                              = "gw1-to-gw2"
+  location                          = azurerm_resource_group.rg1.location
+  resource_group_name               = azurerm_resource_group.rg1.name
+  type                               = "Vnet2Vnet"
+  virtual_network_gateway_id         = azurerm_virtual_network_gateway.gw1.id
+  peer_virtual_network_gateway_id    = azurerm_virtual_network_gateway.gw2.id
+  shared_key                         = local.ipsec_psk
+  enable_bgp                         = false
+}
+
+resource "azurerm_virtual_network_gateway_connection" "gw2_to_gw1" {
+  name                              = "gw2-to-gw1"
+  location                          = azurerm_resource_group.rg2.location
+  resource_group_name               = azurerm_resource_group.rg2.name
+  type                               = "Vnet2Vnet"
+  virtual_network_gateway_id         = azurerm_virtual_network_gateway.gw2.id
+  peer_virtual_network_gateway_id    = azurerm_virtual_network_gateway.gw1.id
+  shared_key                         = local.ipsec_psk
+  enable_bgp                         = false
+}
+
+#####################
+# Outputs
+#####################
+output "vm1_public_ip" {
+  value = azurerm_public_ip.vm1_pip.ip_address
+}
+
+output "vm2_public_ip" {
+  value = azurerm_public_ip.vm2_pip.ip_address
+}
+
+output "vm1_private_ip" {
+  value = azurerm_network_interface.vm1_nic.private_ip_address
+}
+
+output "vm2_private_ip" {
+  value = azurerm_network_interface.vm2_nic.private_ip_address
+}
+
+output "ssh_private_key_pem" {
+  value     = tls_private_key.vm.private_key_pem
+  sensitive = true
+}
